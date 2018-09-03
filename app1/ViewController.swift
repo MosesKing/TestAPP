22fe61fbebb8c1f8188236e364b6ae641a7980ba
MosesKing <mkc42@mail.missouri.edu>
2018-09-02 20:54:45 -0500

    Initial Commit

---
+++ app1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app1
+//
+//  Created by moe on 9/2/18.
+//  Copyright © 2018 m&s. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
